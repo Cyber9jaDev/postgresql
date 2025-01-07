a5e0705e4e0e4801b86b9e047ceb600eb1caee7c
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-07 16:24:09 +0000

    minor update

--- filter.sql
+++ filter.sql
@@ -1,1 +1,11 @@
+SELECT * FROM cities;
 SELECT name, area FROM cities WHERE area BETWEEN 10000 AND 50000;
+
+-- Cities present in this list 
+SELECT name, area FROM cities WHERE name IN ('Tokyo', 'CArt', 'New York');
+
+-- Cities not present in this list
+SELECT name, area FROM cities WHERE name NOT IN ('Tokyo', 'Seoul' );
+
+SELECT name, area FROM cities WHERE area NOT IN (11800, 17800 ) AND name = 'Tokyo';
+
