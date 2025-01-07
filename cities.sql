ee74d146c4f6f24198c63de46a1ff54db419f77a
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-07 14:08:59 +0000

    first push

---
+++ cities.sql
@@ -0,0 +1,7 @@
+INSERT INTO cities (name, country, population, area) 
+  VALUES 
+    ('CArt', 'AFG', 10000, 17800),
+    ('Tokyo', 'Japan', 10000, 17800),
+    ('Seoul', 'Korea', 10000, 17800);
+
+SELECT name FROM cities;
