4572ca925c4df73e435e8ed30c6986729a85a424
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-08 11:28:16 +0000

    update and delete a record

--- filter.sql
+++ filter.sql
@@ -1,3 +1,4 @@
+INSERT INTO cities (name, area, country, population) VALUES('Ibadan', 1000, 'Nigeria', 5000000);
 SELECT * FROM cities;
 SELECT name, area FROM cities WHERE area BETWEEN 10000 AND 50000;
 
@@ -7,5 +8,7 @@ SELECT name, area FROM cities WHERE name IN ('Tokyo', 'CArt', 'New York');
 -- Cities not present in this list
 SELECT name, area FROM cities WHERE name NOT IN ('Tokyo', 'Seoul' );
 
-SELECT name, area FROM cities WHERE area NOT IN (11800, 17800 ) AND name = 'Tokyo';
+SELECT name, area FROM cities WHERE area NOT IN (11800, 27800) OR name = 'Tokyo';
 
+-- Updating records in a table
+UPDATE cities SET area = 50000 WHERE name = 'Tokyo';
