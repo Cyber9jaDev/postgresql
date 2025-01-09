5fa16174f638fe3358c37519870c64c95695b116
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-09 19:13:04 +0000

    minor fix

--- users.sql
+++ users.sql
@@ -9,14 +9,24 @@ CREATE TABLE users (
 CREATE TABLE photos (
   id SERIAL PRIMARY KEY,
   url VARCHAR(200) NOT NULL,
-  user_id INTEGER REFERENCES users(id)
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 INSERT INTO users (username) VALUES ('Luis7'), ('Messi10'), ('Steven8'), ('Torres9');
 
 -- INSERT INTO photos (url, user_id) VALUES ('url 788', 1);
-INSERT INTO photos (url, user_id) VALUES ('url1', 1), ('url2', 2), ('url3', 3), ('url4', 4);
+INSERT INTO photos (url, user_id) 
+  VALUES 
+    ('url 11', 1), 
+    ('url 22', 2), 
+    ('url 33', 3), 
+    ('url 44', 4);
+    
 
 SELECT url FROM photos WHERE user_id = 1;
 
 SELECT url, username FROM photos JOIN users ON users.id = photos.user_id WHERE user_id = 1;
+
+DELETE FROM users WHERE id = 1;
+
+DROP TABLE photos;
